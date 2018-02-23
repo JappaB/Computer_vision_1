7a8ec0551bd58285d544ce782ca5a1084078999f
JappaB <jcsbakker@gmail.com>
2018-02-23 14:53:32 +0100

    gaussian filters af

--- Assignment_2/Part-1: Image Enhancement/gauss1D.m
+++ Assignment_2/Part-1: Image Enhancement/gauss1D.m
@@ -3,5 +3,13 @@ function G = gauss1D( sigma , kernel_size )
     if mod(kernel_size, 2) == 0
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
-    %% solution
+    % Compute the range
+    plus_minus_range = floor(kernel_size / 2);
+    kernel = (-plus_minus_range:plus_minus_range);
+    
+    % Compute the 1d Gaussian
+    G = 1 / (sigma * sqrt(2 * pi)) * exp(- kernel .^ 2 / (2 * sigma ^ 2));
+    % Normalize the filter so the intensity doesn't change
+    G = G / sum(G)
+    
 end
--- Assignment_2/Part-1: Image Enhancement/gauss2D.m
+++ Assignment_2/Part-1: Image Enhancement/gauss2D.m
@@ -1,3 +1,7 @@
 function G = gauss2D( sigma , kernel_size )
-    %% solution
+    % solution
+    % 1D Gaussian
+    G1D = gauss1D(sigma, kernel_size);
+    % 2D Gaussian
+    G = transpose(G1D) * G1D
 end
