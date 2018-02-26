1619e210304c2f3bf0f9c96476faded73bac19d1
aronhammond <hammond756@live.nl>
2018-02-26 16:35:12 +0100

    stuff for denoising guassian noise

--- Assignment_2/psnr.m
+++ Assignment_2/psnr.m
@@ -2,9 +2,9 @@
 clear all
 close all
 
-image1_original = imread('images/image1.jpg');
-image1_sp = imread('images/image1_saltpepper.jpg');
-image1_gauss = imread('images/image1_gaussian.jpg');
+image1_original = im2double(imread('images/image1.jpg'));
+image1_sp = im2double(imread('images/image1_saltpepper.jpg'));
+image1_gauss = im2double(imread('images/image1_gaussian.jpg'));
 
 %% 4.2 Report the PSNR on image1
 
@@ -16,11 +16,59 @@ psnr_image1_gauss = myPSNR(image1_original, image1_gauss)
 
 %% 4.3 Denoise images
 
-image1_sp_denoised = denoise(image1_sp, 'median', [5 5]);
-image1_gauss_denoised = denoise(image1_gauss, 'gaussian', 0.5, 5);
+% Question 7.1
+kernel_sizes = [[3 3]; [5 5]; [7 7]];
 
+[h,w] = size(image1_sp);
+n = size(kernel_sizes, 1);
+
+images_denoised_box = zeros(h, w, n);
+images_denoised_median = zeros(h, w, n);
+
+% Denoise saltpepper images with box/median
+for i = 1:n
+    images_denoised_box(:,:,i) = denoise(image1_sp, 'box', kernel_sizes(i, :));
+    images_denoised_median(:,:,i) = denoise(image1_sp, 'median', kernel_sizes(i, :));
+end
+
+% Denoise gaussian images with gaussian 0.5, 1 and 2
+sigmas = [0.5; 1; 2];
+n = size(sigmas, 1);
+images_denoised_gauss = zeros(h, w, n);
+ 
+for i = 1:n
+    images_denoised_gauss(:, :, i) = denoise(image1_gauss, 'gaussian', sigmas(i), [7 7]);
+end
+%% Plot denoised images
+% Plot median filter results
 figure
-subplot(2,2,1), imshow(image1_sp), title('saltpepper');
-subplot(2,2,2), imshow(image1_sp_denoised), title('saltpepper denoised');
-subplot(2,2,3), imshow(image1_gauss), title('gaussian');
-subplot(2,2,4), imshow(image1_gauss_denoised), title('gaussian denoised');
+for i = 1:n
+    subplot(1, n, i); imshow(images_denoised_median(:,:,i));
+    sizes = kernel_sizes(i, :);
+    title(string(sizes(1)) + "x" + string(sizes(2)));
+end
+
+% Plot boxfilter results
+figure
+for i = 1:n
+    subplot(1, n, i); imshow(images_denoised_box(:,:,i));
+    sizes = kernel_sizes(i, :);
+    title(string(sizes(1)) + "x" + string(sizes(2)));
+end
+
+%% Plot gaussian results
+figure
+for i = 1:n
+    subplot(1, n, i); imshow(images_denoised_gauss(:,:,i));
+    sigma = sigmas(i);
+    title(string(sigma));
+end
+%% Calculate PSN for denoised images
+
+PSNR_median = zeros(n);
+PSNR_box = zeros(n);
+
+for i = 1:n
+    PSNR_median = myPSNR(image1_original, images_denoised_median(:, :, i))
+    PSNR_box = myPSNR(image1_original, images_denoised_box(:, :, i))
+end
