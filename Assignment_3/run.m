476da4949c9be51e297e88b47d1847f460d0c634
aronhammond <hammond756@live.nl>
2018-03-06 22:27:26 +0100

    removed plot from run function

--- Assignment_3/run.m
+++ Assignment_3/run.m
@@ -8,11 +8,17 @@ pingpong_image = rgb2gray(imread('pingpong/0000.jpeg'));
 %% Plot points of interest
 
 close all
-threshold = 0.001;
+threshold_ping = 3e-6;
+threshold_pers = 5e-7;
 
 % Swapping c/r fixed pard of the problem....
-[H_pers, r_pers, c_pers] = harris_corner_detector(person_image, threshold);
-[H_ping, r_ping, c_ping] = harris_corner_detector(pingpong_image, threshold);
+[H_pers, r_pers, c_pers] = harris_corner_detector(person_image, threshold_pers);
+[H_ping, r_ping, c_ping] = harris_corner_detector(pingpong_image, threshold_ping);
+
+%% Test rotation invariance
+angle = random('uniform', 1, 360)
+rotated_person = imrotate(person_image, angle);
+[H_rotated, r_rotated, c_rotated] = harris_corner_detector(rotated_person, threshold_pers);
 
 %% Lucas-Kanade
 close all
