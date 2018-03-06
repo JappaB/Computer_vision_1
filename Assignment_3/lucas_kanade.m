d9c1145e61a7bc46b4999ef2d00210a66d215846
aronhammond <hammond756@live.nl>
2018-03-06 23:33:34 +0100

    define regions by center and build Axb inside of the loop for each region

--- Assignment_3/lucas_kanade.m
+++ Assignment_3/lucas_kanade.m
@@ -1,8 +1,8 @@
-function lucas_kanade(image1, image2, time_step, interest_points)
+function [Vx Vy] = lucas_kanade(image1, image2, time_step, i_r, i_c)
 
 % hyper params
 region_size = 15;
-
+boundary_offset = region_size - ceil(region_size/2);
 
 % Convert the image to grayscale if needed
 
@@ -15,32 +15,34 @@ else
 end 
 
 % Divide input images on non-overlapping regions, each region being 15×15
-[ rows, cols ] = size(image1_gray) %Images should be equal size
-num_rows = floor(rows/region_size) % Floor (vs ceil) to prevent going out of the image
-num_cols = floor(cols/region_size)
+[ rows, cols ] = size(image1_gray); %Images should be equal size
+num_rows = floor(rows/region_size); % Floor (vs ceil) to prevent going out of the image
+num_cols = floor(cols/region_size);
 
-% Indices of the boundaries between regions
-row_region_bounds = 1:region_size:rows;
-column_region_bounds = 1:region_size:cols;
-
-
-if nargin < 4  
-    interest_points_rows =  (ceil(region_size/2)):region_size+1:rows
-    interest_points_columns = (ceil(region_size/2)):region_size+1:cols
+% Define hard-coded regions to track
+if nargin < 4
+    % define diagonal [ a b c ], [ a b c ]
+    i_r = ((ceil(region_size/2)):region_size+1:rows)';
+    i_c = ((ceil(region_size/2)):region_size+1:cols);
+    
+    % repeat the row indices and reshape to [a a a b b b c c c]
+    region_centers_row = repmat(i_r, 1, num_rows)';
+    region_centers_row = region_centers_row(:)';
+    
+    % repeat the col indices and reshape to [a b c a b c a b c]
+    region_centers_col = repmat(i_c, 1, num_cols)';
+    region_centers_col = region_centers_col(:)';
 end
 
 % Test plot to show boundaries of regions
 figure;
 imshow(image1);
 hold on;
-plot(column_region_bounds, row_region_bounds, 'rd');
+plot(region_centers_col, region_centers_row, 'rd');
 hold off;
-
 
 % 2. For each region compute A, A T and b. Then, estimate optical flow as
 % given in Equation 20.
-
-
 
 % First create a vector for the T dimension
 % T = ones(rows, cols);
@@ -50,34 +52,40 @@ hold off;
 I = image1_gray;
 I(:,:,2) = image2_gray;
 
-% Initiate Matrix A
-A = zeros(rows, cols)
-b = zeros()
 % Calculate for each region
-for row = row_region_bounds
-    for col = column_region_bounds
+for row = region_centers_row
+    for col = region_centers_col
+        
         % specify boundaries
-        row_start = min(row,rows);
-        row_stop = min(row + region_size - 1,rows);
-        col_start = min(col, cols);
-        col_stop = min(col + region_size -1, cols);
+        row_start = min(row - boundary_offset,rows);
+        row_stop = min(row + boundary_offset-1,rows);
+        col_start = min(col - boundary_offset, cols);
+        col_stop = min(col + boundary_offset-1, cols);
         
         % Partial derivatives Ix, Iy and It
         % This representation of T assumes a time step of one
-        [Ix, Iy, It]= gradient(I(row_start:row_stop,col_start:col_stop,:))
+        [Ix, Iy, It] = gradient(I(row_start:row_stop,col_start:col_stop,:));
         
         % If the timestep is larger than one, multiply 
         It = It*time_step;
         
-        % Fill A with Ix and Iy
-        A(row_start,col_start) = [Ix Iy]
+        % Unroll gradient intensities to create 1D vectors 
+        Ix = Ix(:);
+        Iy = Iy(:);
+        It = It(:);
         
-        % Fill b
+        % Build elements of system to solve
+        A = [Ix Iy];
+        b = -It;
         
+        % Solve the equation using the pseudo-inverse of A
+        V = pinv(A) * b
         
-        
-        
-    end
+        % TODO: Assign V(1) and V(2) to vectors OUTSIDE of the loop
+        Vx = V(1);
+        Vy = V(2);
+                
+        end
 end
 
 
