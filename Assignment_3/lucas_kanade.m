f48d4de2b278295bdedcf7678d6d7e2f99c65f13
JappaB <jcsbakker@gmail.com>
2018-03-07 12:22:50 +0100

    trying to fix the LK bug

--- Assignment_3/lucas_kanade.m
+++ Assignment_3/lucas_kanade.m
@@ -1,5 +1,8 @@
 function [Vx Vy] = lucas_kanade(image1, image2, time_step, i_r, i_c)
 
+
+
+
 % hyper params
 region_size = 15;
 boundary_offset = region_size - ceil(region_size/2);
@@ -35,11 +38,11 @@ if nargin < 4
 end
 
 % Test plot to show boundaries of regions
-figure;
-imshow(image1);
-hold on;
-plot(region_centers_col, region_centers_row, 'rd');
-hold off;
+% figure;
+% imshow(image1);
+% hold on;
+% plot(region_centers_col, region_centers_row, 'rd');
+% hold off;
 
 % 2. For each region compute A, A T and b. Then, estimate optical flow as
 % given in Equation 20.
@@ -52,6 +55,12 @@ hold off;
 I = image1_gray;
 I(:,:,2) = image2_gray;
 
+[Ix,Iy,It] = gradient(I)
+
+% Initiate V, for each pixel in the x and the y direction
+Vx = zeros(num_rows,num_cols);
+Vy = zeros(num_cols,num_cols);
+
 % Calculate for each region
 for row = region_centers_row
     for col = region_centers_col
@@ -62,46 +71,66 @@ for row = region_centers_row
         col_start = min(col - boundary_offset, cols);
         col_stop = min(col + boundary_offset-1, cols);
         
+        windowIx = Ix(row_start:row_stop,col_start:col_stop);
+        windowIy = Iy(row_start:row_stop,col_start:col_stop);
+        windowIt = It(row_start:row_stop,col_start:col_stop);
+        
         % Partial derivatives Ix, Iy and It
         % This representation of T assumes a time step of one
-        [Ix, Iy, It] = gradient(I(row_start:row_stop,col_start:col_stop,:));
+%         [Ix, Iy] = gradient(windowI);
         
+        %TODO Let op: It is nog zero...
+%         It = windowI(:,:,1)-windowI(:,:,2);
+       
         % If the timestep is larger than one, multiply 
         It = It*time_step;
         
         % Unroll gradient intensities to create 1D vectors 
-        Ix = Ix(:);
-        Iy = Iy(:);
-        It = It(:);
+        windowIx = windowIx(:);
+        windowIy = windowIy(:);
+        windowIt = windowIt(:);
         
         % Build elements of system to solve
-        A = [Ix Iy];
-        b = -It;
+        A = [windowIx windowIy];
+        b = -windowIt;
         
         % Solve the equation using the pseudo-inverse of A
-        V = pinv(A) * b
+        V = pinv(A) * b;
         
-        % TODO: Assign V(1) and V(2) to vectors OUTSIDE of the loop
-        Vx = V(1);
-        Vy = V(2);
+        % Find the row and column that belong to the interest point
+        x = find(region_centers_row==row);
+        y = find(region_centers_col==col);
+        
+        
+        %Assign V(1) and V(2) to vectors OUTSIDE of the loop        
+        Vx(x,y) = V(1);
+        Vy(x,y) = V(2);
+        
+        
                 
         end
 end
 
 
 
-
-% Matrix A, Matrix A transposed and vector b
-
-% vector v
-
-
 % 3. When you have estimation for optical flow (V x , V y ) of each region, you
 % should display the results. There is a MATLAB function quiver which
 % plots a set of two-dimensional vectors as arrows on the screen. Try to
 % figure out how to use this to plot your optical flow results.
- 
 
+% size(region_centers_col)
+% size(region_centers_row)
+% size(Vx)
+% size(Vy)
+
+
+
+
+figure();
+imshow(image2);
+hold on;
+quiver(region_centers_row,region_centers_col,Vx,Vy)
+hold off;
 
 
 end
