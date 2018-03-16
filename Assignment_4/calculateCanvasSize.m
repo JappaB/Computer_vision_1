4eb57154e751f3607b804f86d69c4d4339f188af
aronhammond <hammond756@live.nl>
2018-03-16 17:53:03 +0100

    added some comments

--- Assignment_4/calculateCanvasSize.m
+++ Assignment_4/calculateCanvasSize.m
@@ -1,18 +1,23 @@
 function [h, w, xoffset, yoffset] = calculateCanvasSize(image, A, method)
 %CALCULATECANVASSIZE Compute boundaries of transformed corners of the
 % of the input image.
+
+    % Transform corners of image
     corners = [1 1;size(image, 2) 1; 1 size(image, 1); size(image, 2) size(image, 1)]';
     newCorners = A * [corners; ones(1, size(corners, 2))];
     
+    % Different behaviour based on application
     if method == "stitch"
+        % Return max
         h = max(newCorners(2,:));
         w = max(newCorners(1,:));
     else
+        % Return difference between extrema
         h = max(newCorners(2,:)) - min(newCorners(2,:));
         w = max(newCorners(1,:)) - min(newCorners(1,:));
     end
     
-
+    % Return offset for aligning the image in the figure
     xoffset = min(newCorners(1,:));
     yoffset = min(newCorners(2,:));
 end
