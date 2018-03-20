aa49727382484892a5490336c655b4dc6889592d
aronhammond <hammond756@live.nl>
2018-03-20 10:58:33 +0100

    run finetune_cnn for all hyper parameter settings

--- FinalProject (2)/part2/main.m
+++ FinalProject (2)/part2/main.m
@@ -1,17 +1,26 @@
-%% main function 
-
-
 %% fine-tune cnn
+clear all
 
-[net, info, expdir] = finetune_cnn();
+for batchSize=[50 100]
+    for numEpochs=[40 80 100]
+        [net, info, expdir] = finetune_cnn(batchSize, numEpochs);
+    end
+end
 
 %% extract features and train svm
 
-% TODO: Replace the name with the name of your fine-tuned model
-nets.fine_tuned = load(fullfile(expdir, 'your_new_model.mat')); nets.fine_tuned = nets.fine_tuned.net;
-nets.pre_trained = load(fullfile('data', 'pre_trained_model.mat')); nets.pre_trained = nets.pre_trained.net; 
-data = load(fullfile(expdir, 'imdb-caltech.mat'));
+for batchSize=[50 100]
+    for numEpochs=[40 80 100]
+        % TODO: Replace the name with the name of your fine-tuned model
+        nets.fine_tuned = load(fullfile('models','batch-%i_epoch-%i', 'net-epoch-50.mat')); nets.fine_tuned = nets.fine_tuned.net;
+        nets.pre_trained = load(fullfile('data', 'pre_trained_model.mat')); nets.pre_trained = nets.pre_trained.net; 
+        data = load(fullfile(expdir, 'imdb-caltech.mat'));
+
+        % NB: liblinear-2.1 should be added to path, otherwise train/predict
+        % are not known by matlab
+        train_svm(nets, data);
+    end
+end
 
 
-%%
-train_svm(nets, data);
+
