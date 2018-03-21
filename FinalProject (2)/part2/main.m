2d59a09db5b67efa1ddf8b426d48b2e86d7b057a
aronhammond <hammond756@live.nl>
2018-03-21 22:35:52 +0100

    code for running and plotting tsne

--- FinalProject (2)/part2/main.m
+++ FinalProject (2)/part2/main.m
@@ -6,15 +6,16 @@ for batchSize=[50 100]
         [net, info, expdir] = finetune_cnn(batchSize, numEpochs);
     end
 end
-
 %% extract features and train svm
 
 for batchSize=[50 100]
     for numEpochs=[40 80 100]
         % TODO: Replace the name with the name of your fine-tuned model
-        nets.fine_tuned = load(fullfile('models','batch-%i_epoch-%i', 'net-epoch-50.mat')); nets.fine_tuned = nets.fine_tuned.net;
+        model_setting = sprintf('batch-%i_epochs-%i', batchSize, numEpochs);
+        last_epoch = sprintf('net-epoch-%i.mat', numEpochs);
+        nets.fine_tuned = load(fullfile('models',model_setting, last_epoch)); nets.fine_tuned = nets.fine_tuned.net;
         nets.pre_trained = load(fullfile('data', 'pre_trained_model.mat')); nets.pre_trained = nets.pre_trained.net; 
-        data = load(fullfile(expdir, 'imdb-caltech.mat'));
+        data = load(fullfile('data/cnn_assignment-lenet', 'imdb-caltech.mat'));
 
         % NB: liblinear-2.1 should be added to path, otherwise train/predict
         % are not known by matlab
@@ -22,5 +23,22 @@ for batchSize=[50 100]
     end
 end
 
+%% T-SNE batchSize = 50; numEpochs = 40;
 
+data = load(fullfile('data/cnn_assignment-lenet', 'imdb-caltech.mat'));
 
+fine_reduced = reduce_features(data, 'models/batch-50_epochs-40/net-epoch-40.mat');
+pre_reduced = reduce_features(data, 'data/pre_trained_model.mat');
+
+%% Visualze reduced vectors
+
+training_set = data.images.set == 1;
+test_set = data.images.set == 2;
+
+labels = [data.images.labels(1, training_set) data.images.labels(1, test_set)];
+labels = transpose(labels);
+
+subplot(2,1,1); title('Fine-tuned features');
+gscatter(fine_reduced(:,1), fine_reduced(:,2), labels);
+subplot(2,1,2); title('Pre-trained features');
+gscatter(pre_reduced(:,1), pre_reduced(:,2), labels);
---
+++ FinalProject (2)/part2/reduce_features.m
@@ -0,0 +1,18 @@
+function X = reduce_features( data, path_to_net )
+    % REDUCE_FEATURES runs T-SNE with default parameters on the provided data set.
+    % if labels = [], no intermediate results are plotted
+
+    % Load network
+    net = load(path_to_net);
+    net = net.net;
+
+    % set loss function to softmax, because we are just evaluating
+    % loaded setting requires labels
+    net.layers{end}.type = 'softmax';
+    
+    [training, test] = get_svm_data(data, net);
+    features = [training.features; test.features];
+    labels = [training.labels; test.labels];
+
+    X = tsne(full(features), labels);
+end
