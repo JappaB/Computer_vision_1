b1608543d0538f3697fb02887e83bf6d795d2349
aronhammond <hammond756@live.nl>
2018-03-22 18:43:10 +0100

    train svm works

--- FinalProject (2)/BoW.m
+++ FinalProject (2)/BoW.m
@@ -25,48 +25,22 @@ centers = vl_ikmeans(descriptors, k, 'method', 'elkan', 'verbose');
 toc
 
 % Sample a part of the features, so k-means can converge
+%% Load unseen images for training
+
+n_samples = 20;
+[training_set, ~] = load_images_bow("train", n_samples, used_images);
+[test_set, ~] = load_images_bow("test", 50, cell(1,4));
+
 %% Quantize features and represent image by frequencies
+[training_features, training_labels] = create_binary_dataset(training_set, centers, colorspace, dense);
+[test_features, test_labels] = create_binary_dataset(test_set, centers, colorspace, dense);
 
-% Quantize Features Using Visual Vocabulary  
-% extract Sift descriptors again (from other images)
-samples_per_catagory2 = 10
-[image_set2, used_images2] = load_images_bow("train", samples_per_catagory, used_images)
-
-% Initialize an empty cell array to store the hist count per catagory per
-% image
-histcounts_array = {};
-% For each catagory, for each image
-for i = 1:4;
-    for j= 1: size(image_set2,2);
-        
-        % get the descriptors
-        descriptors2 = extract_sift_features_per_image(image_set2{i,j}, colorspace, dense);
-
-        % Project the data to the k-means clusters
-        quantized_features = vl_ikmeanspush(descriptors2, centers);
-
-        % Representing images by frequencies of visual words (normalized
-        % between 0 and 1)
-        normed_counts = histcounts(quantized_features,400, 'Normalization', 'probability');
-
-        histcounts_array{i,j} = normed_counts;
-        
-    end
-end
 %% Train SVM (per class)
-%nX400 feature vector and a nx1 (met [1-4]) label vector
 
-% Unpack histcounts
-
-[predictions, accuracy] = get_predictions(data)
-
-best = train(data.trainset.labels, data.trainset.features, '-C -s 0');
-model = train(data.trainset.labels, data.trainset.features, sprintf('-c %f -s 0', best(1))); % use the same solver: -s 0
-[predictions, accuracy, ~] = predict(data.testset.labels, data.testset.features, model);
-
-
+classifiers = [];
+for class = [1]
+    model = train(double(training_labels == class), sparse(training_features), '-s 0');
+    classifiers = [classifiers model];
 end
-
-
 
 %% Evaluation 
---
+++ FinalProject (2)/create_binary_dataset.m
@@ -0,0 +1,29 @@
+function [features, labels] = create_binary_dataset( image_set, centers, colorspace, dense )
+    %CREATE_BINARY_DATASET matrix containing the sift features for
+    % the images in image_set
+    % Quantize Features Using Visual Vocabulary  
+
+% Initialize an empty arrays to store the hist count and labels
+labels = [];
+features = [];
+
+% For each catagory, for each image
+for i = 1:size(image_set, 1)
+    for j= 1:size(image_set, 2)
+        
+        % get the descriptors
+        descriptors = extract_sift_features_per_image(image_set{i,j}, colorspace, dense);
+
+        % Project the data to the k-means clusters
+        quantized_features = vl_ikmeanspush(descriptors, centers);
+
+        % Representing images by frequencies of visual words (normalized
+        % between 0 and 1)
+        normed_counts = histcounts(quantized_features, size(centers, 2), 'Normalization', 'count');
+
+        features = [features; normed_counts];
+        labels = [labels; i];
+    end
+end
+    
+end
