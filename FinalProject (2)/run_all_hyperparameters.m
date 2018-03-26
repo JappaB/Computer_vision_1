3b3115c5fe1881a4a70f77ff84d27b57a4c5d447
aronhammond <hammond756@live.nl>
2018-03-26 15:41:12 +0200

    added svm training to loop

--- FinalProject (2)/run_all_hyperparameters.m
+++ FinalProject (2)/run_all_hyperparameters.m
@@ -1,10 +1,7 @@
 % Run all
 % Hyperparameters
-n_training_samples = [50,100,250,200]
+
 % colorspaces = ["gray", "opponent", "RGB", "normalized_rgb"] 
-colorspaces = ["gray"]
-vocabulary_sizes = [400, 800, 1600, 2000, 4000] % k
-dense_or_not = [false, true]
 
 vl_threads
 %kernel_choices = ["linear", 'rbf']
@@ -18,63 +15,66 @@ clear all
 close all
 
 % Run everything for gray, use the rest for the colors
-for i = 1:size(n_training_samples,1);
-    samples_per_catagory = n_training_samples(i)
-    
-    % Extract images
-    [image_set, used_images] = load_images_bow("train", samples_per_catagory)
-    
-    for j = 1:size(colorspaces,1);
-        for k = 1:size(dense_or_not,1);  
-            colorspace = colorspaces(j)
-            dense = dense_or_not(k)
-            
+% for n_training_samples = [50,100,250,200]
+%     for k = [400, 800, 1600, 2000, 4000]
+%         for dense = [false, true]
+for n_training_samples = [50]
+    for k = [400]
+        for dense = [false]    
+            % Extract images
+            [image_set, used_images] = load_images_bow("train", n_training_samples);
+
+            colorspace = "gray";
+
             % Extract SIFT features
-            
+
             tic
             descriptors = extract_sift_features(image_set, colorspace, dense);
             time_extract_features = toc
-            
-            
-            for l = 1:size(vocabulary_sizes,1);
-                vocabulary_size = vocabulary_sizes(l) % k   
-                
-                tic
-                % Cluster    
-                % use elkan to speed up
-                centers = vl_ikmeans(descriptors, k, 'method', 'elkan', 'verbose');
-                time_to_cluster = toc
-                
-                
-%               build vocabulary
-                
-                
-                for m = 1:size(kernel_choices,1);
-                    
 
-                    % Train SVM
-                    kernel_choice = kernel_choices(m)
-                                     
-                    % Classify test images and calculate 
-                    
-                    % For each descriptor type (combination of step-size
-                    
-                    
-                    
-                    % Doe sprintf voor de variable name en sla op als .mat
-                    % (save('A.MAT','A')
-                    
-                    
-                    % Write the times to an excel file
-                    xlswrite(filename,M)
-                    % Write the scores per image per SVM-classifier (with the hyerparameters) to an
-                    % excel 
-                    % file
-                    
-                    % Write 
-                    
+
+            tic
+            % Cluster    
+            % use elkan to speed up
+            centers = vl_ikmeans(descriptors, k, 'method', 'elkan', 'verbose');
+            time_to_cluster = toc
+            
+            [training_set, ~] = load_images_bow("train", n_training_samples, used_images);
+            [test_set, ~] = load_images_bow("test", 50, cell(1,4));
+
+            % Quantize features and represent image by frequencies
+            [training_features, training_labels] = create_binary_dataset(training_set, centers, colorspace, dense);
+            [test_features, test_labels] = create_binary_dataset(test_set, centers, colorspace, dense);
+
+            % Train SVM (per class)
+
+            predictions = [];
+            accuracies = [];
+            
+            for class = [1 2 3 4]
+
+                % Shuffle data
+                new_order = randperm(size(training_features, 1));
+                training_labels = training_labels(new_order);
+                training_features = training_features(new_order,:);
+
+                while training_labels(1,1) == class
+                    new_order = randperm(size(training_features, 1));
+                    training_labels = training_labels(new_order);
+                    training_features = training_features(new_order,:);
                 end
+
+                model = train(double(training_labels == class), sparse(training_features), '-s 0');
+                [preds,acc,probs] = predict(double(test_labels == class), sparse(test_features), model, '-b 1');
+                predictions = [predictions probs(:,2)];
+                accuracies = [accuracies acc];
             end
+            
+            filename = sprintf('predictions/n-%i_k-%i_dense-%i.mat', n_training_samples, k, dense);
+            save(filename, '-mat', 'predictions');
+
+            filename = sprintf('accuracies/n-%i_k-%i_dense-%i.mat', n_training_samples, k, dense);
+            save(filename, '-mat', 'accuracies');
         end
     end
 end
