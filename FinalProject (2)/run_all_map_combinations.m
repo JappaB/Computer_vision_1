9740d04d9825faa7f0c3f0a1bbe168823655bd39
aronhammond <hammond756@live.nl>
2018-04-04 22:26:50 +0200

    script to run mAP experiments

---
+++ FinalProject (2)/run_all_map_combinations.m
@@ -0,0 +1,47 @@
+% Run all
+% Hyperparameters
+
+vl_threads
+
+% BONUS TODO: we kunnen nog harris corner features gebruiken => is slechter maar
+% sneller => tic toc opslaan
+
+% BONUS TODO: nog k-neares neighbour ipv k-means: https://nl.mathworks.com/help/stats/knnsearch.html
+
+clear all
+close all
+
+
+% Vary Dense/Keypoint
+k = 400;
+n_training_samples = 50;
+colorspace = "gray";
+for dense = [false true]
+    train_and_save_svm(n_training_samples, k, colorspace, dense);
+end
+
+% Vary k
+dense = false;
+n_training_samples = 50;
+colorspace = "gray";
+for k = [400 800 1600 2000]
+   train_and_save_svm(n_training_samples, k, colorspace, dense, 500);
+end
+
+
+% Vary n
+k = 400;
+dense = false;
+colorspace = "gray";
+for n_training_samples = [ 50 100 200 250 ]
+    train_and_save_svm(n_training_samples, k, colorspace, dense);
+end
+
+
+% Vary Colorspace
+k = 400;
+n_training_samples = 50;
+dense = false;
+for colorspace = ["gray", "opponent", "RGB", "normalized_rgb"]
+    train_and_save_svm(n_training_samples, k, colorspace, dense);
+end
