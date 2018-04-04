bd68cca9febdcd56a98f19811e1ee26987a5cc24
aronhammond <hammond756@live.nl>
2018-04-04 22:36:50 +0200

    uitgecomment wat ik zelf ga runnen

--- FinalProject (2)/run_all_map_combinations.m
+++ FinalProject (2)/run_all_map_combinations.m
@@ -13,22 +13,22 @@ close all
 
 
 % Vary Dense/Keypoint
-k = 400;
-n_training_samples = 50;
-colorspace = "gray";
-for dense = [false true]
-    train_and_save_svm(n_training_samples, k, colorspace, dense);
-end
+% k = 400;
+% n_training_samples = 50;
+% colorspace = "gray";
+% for dense = [false true]
+%     train_and_save_svm(n_training_samples, k, colorspace, dense);
+% end
+% 
+% % Vary k
+% dense = false;
+% n_training_samples = 50;
+% colorspace = "gray";
+% for k = [400 800 1600 2000]
+%    train_and_save_svm(n_training_samples, k, colorspace, dense, 500);
+% end
 
-% Vary k
-dense = false;
-n_training_samples = 50;
-colorspace = "gray";
-for k = [400 800 1600 2000]
-   train_and_save_svm(n_training_samples, k, colorspace, dense, 500);
-end
-
-
+% @ JAPSER: ALLEN HIERONDER RUNNEN, IK RUN DE BOVENSTE TWEE
 % Vary n
 k = 400;
 dense = false;
