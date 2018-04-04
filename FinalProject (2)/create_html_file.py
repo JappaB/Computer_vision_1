71d229840fb8fbca5aaad343b252fb924925ff04
aronhammond <hammond756@live.nl>
2018-04-04 22:21:37 +0200

    writes entire filled in html page to file

--- FinalProject (2)/create_html_file.py
+++ FinalProject (2)/create_html_file.py
@@ -2,8 +2,8 @@ import numpy as np
 import scipy.io
 
 def rank_paths(model_name):
-    image_folders = ['Caltech4/ImageData/airplanes_test', 'Caltech4/ImageData/cars_test',
-                   'Caltech4/ImageData/faces_test' , 'Caltech4/ImageData/motorbikes_test']
+    image_folders = ['../Caltech4/ImageData/airplanes_test', '../Caltech4/ImageData/cars_test',
+                   '../Caltech4/ImageData/faces_test' , '../Caltech4/ImageData/motorbikes_test']
     image_paths = []
 
     for i, class_id in enumerate([1]*50 + [2]*50 + [3]*50 + [4]*50):
@@ -33,4 +33,54 @@ def create_html_table_body(ranked_paths):
 
     return result
 
-print(create_html_table_body(rank_paths('stride-20_n-5_k-40_dense-0_colorspace-gray')))
+def create_html_meta_table(model_name):
+    with_string = model_name.split("_")
+    settings = {x.split("-")[0] : x.split("-")[1] for x in with_string}
+    settings['dense'] = "DENSE" if settings['dense'] == "1" else "KEYPOINT"
+
+    result = f"<table><tr><th>SIFT step size</th><td>{settings['stride']} px</td></tr><tr><th>SIFT block sizes</th><td>3 pixels</td></tr><tr><th>SIFT method</th><td>{settings['dense']}-SIFT</td></tr><tr><th>Vocabulary size</th><td>{settings['k']} words</td></tr><tr><th>Vocabulary fraction</th><td>1</td></tr><tr><th>SVM training data</th><td>{settings['n']} positive, 50 negative per class</td></tr><tr><th>SVM kernel type</th><td>Linear</td></tr></table>"
+
+    return result
+
+def create_html_table_header(avg_p):
+    # TODO: read avg_p values
+
+    result = f"<thead><tr><th>Airplanes (AP: {avg_p[0]})</th><th>Cars (AP: {avg_p[1]})</th><th>Faces (AP: {avg_p[2]})</th><th>Motorbikes (AP: {avg_p[3]})</th></tr></thead>"
+
+    return result
+
+def create_html_table_title(mAP):
+    result = f"<h1>Prediction lists (MAP: {mAP})</h1>"
+    return result
+
+def create_page(model_name, mAP, avg_precision):
+    result = "<!DOCTYPE html>\
+    <html lang=\"en\">\
+      <head>\
+        <meta charset=\"utf-8\">\
+        <title>Image list prediction</title>\
+      <style type=\"text/css\">\
+       img {\
+         width:200px;\
+       }\
+      </style>\
+      </head>\
+      <body>\
+    <h2>Jasper Bakker, Aron Hammond</h2>\
+    <h1>Settings</h1>"
+
+    result += create_html_meta_table(model_name)
+    result += create_html_table_title(mAP)
+    result += "<h3><font color=\"red\">Following are the ranking lists for the four categories. Please fill in your lists.</font></h3>\
+    <h3><font color=\"red\">The length of each column should be 200 (containing all test images).</font></h3>"
+    result += "<table>"
+    result += create_html_table_header(avg_precision)
+    result += create_html_table_body(rank_paths(model_name))
+    result += "</table>\
+      </body>\
+    </html>"
+
+    with open("html/" + model_name.split('.')[0] + ".html", 'w') as f:
+        f.write(result)
+
+create_page('stride-20_n-5_k-40_dense-0_colorspace-gray', 1.0, [1.0, 1.0, 1.0, 1.0])
